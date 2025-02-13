c616723e4d1684a89c45838c0b15633f0cc10374
Vlad <v.redula@faradaygroup.com>
2025-02-13 15:18:26 +0900

    Adding aditional properties and optional attributes

--- ZaicoApiInteractor/Models/Item.cs
+++ ZaicoApiInteractor/Models/Item.cs
@@ -5,6 +5,18 @@
         public int id { get; set; }
         public string title { get; set; }
         public float quantity { get; set; }
+        public float logical_quantity { get; set; }
+        public string unit { get; set; }
+        public string category { get; set; }
+        public string state { get; set; }
         public string place { get; set; }
+        public string etc { get; set; }
+        public string code { get; set; }
+        public string group_tag { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public string user_group { get; set; } = "Base group";
+
+        public List<OptionalAttribute> optional_attributes { get; set; } = new List<OptionalAttribute>();
     }
 }
---
+++ ZaicoApiInteractor/Models/OptionalAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace ZaicoApiInteractor.Models
+{
+    public class OptionalAttribute
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+}
