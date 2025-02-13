a1f5b1e445a2d29705e7abfeb9080479304b3e9c
Vlad <v.redula@faradaygroup.com>
2025-02-13 15:14:45 +0900

    added db context and mysql

---
+++ ZaicoApiInteractor/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZaicoApiInteractor.Models;
+
+namespace ZaicoApiInteractor
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
