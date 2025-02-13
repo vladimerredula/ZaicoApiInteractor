5a75efea3bbe02b7103d20713225f9de463b5abc
Vlad <v.redula@faradaygroup.com>
2025-02-13 15:12:51 +0900

    Added user model

---
+++ ZaicoApiInteractor/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZaicoApiInteractor.Models
+{
+    public class User
+    {
+        [Key]
+        [Display(Name = "Personnel ID")]
+        public int Personnel_ID { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR")]
+        [Display(Name = "Username")]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR")]
+        [Display(Name = "Password")]
+        [StringLength(20)]
+        public string Password { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [Display(Name = "First name")]
+        [StringLength(50)]
+        public string? First_name { get; set; }
+
+        [Column(TypeName = "VARCHAR")]
+        [Display(Name = "Last name")]
+        [StringLength(50)]
+        public string? Last_name { get; set; }
+        public string? Email { get; set; }
+        public string? ApiToken { get; set; }
+
+        public int Status { get; set; } = 0;
+        public DateTime? Last_password_changed { get; set; }
+    }
+}
