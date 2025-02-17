c9f43aef9db8191b8612f46ae501e99af29e40e1
Vlad <v.redula@faradaygroup.com>
2025-02-17 17:24:13 +0900

    Added name and dictionary properties

--- ZaicoApiInteractor/Models/Item.cs
+++ ZaicoApiInteractor/Models/Item.cs
@@ -1,22 +1,65 @@
-﻿namespace ZaicoApiInteractor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ZaicoApiInteractor.Models
 {
     public class Item
     {
+        [Display(Name = "Inventory ID")]
         public int id { get; set; }
-        public string title { get; set; }
-        public float? quantity { get; set; }
-        public float? logical_quantity { get; set; }
-        public string? unit { get; set; }
-        public string? category { get; set; }
-        public string? state { get; set; }
-        public string? place { get; set; }
-        public string? etc { get; set; }
-        public string? code { get; set; }
-        public string? group_tag { get; set; }
-        public string? created_at { get; set; }
-        public string? updated_at { get; set; }
-        public string? user_group { get; set; } = "Base group";
 
+        [Display(Name = "Title")]
+        public string title { get; set; }
+
+        [Display(Name = "Quatity")]
+        public float? quantity { get; set; }
+
+        [Display(Name = "Logical quantity")]
+        public float? logical_quantity { get; set; }
+
+        [Display(Name = "Unit")]
+        public string? unit { get; set; }
+
+        [Display(Name = "Category")]
+        public string? category { get; set; }
+
+        [Display(Name = "State")]
+        public string? state { get; set; }
+
+        [Display(Name = "Place")]
+        public string? place { get; set; }
+
+        [Display(Name = "Notes")]
+        public string? etc { get; set; }
+
+        [Display(Name = "QR code/Barcode")]
+        public string? code { get; set; }
+
+        [Display(Name = "Group Tag")]
+        public string? group_tag { get; set; }
+
+        [Display(Name = "Created at")]
+        public string? created_at { get; set; }
+
+        [Display(Name = "Updated at")]
+        public string? updated_at { get; set; }
+
+        [Display(Name = "Updated by")]
+        public string? update_user_name { get; set; }
+
+        [Display(Name = "user group")]
+        public string? user_group { get; set; } = "Base group";
+        public InventoryHistory? inventory_history { get; set; }
+        public ItemImage? item_image { get; set; }
         public List<OptionalAttribute>? optional_attributes { get; set; } = new List<OptionalAttribute>();
+    }
+
+    public class InventoryHistory
+    {
+        public string? memo { get; set; }
+    }
+
+    public class ItemImage
+    {
+        public string? url { get; set; }
     }
 }
